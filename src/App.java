260090fa0b6e6b77fbfcfd3bc9cddc91c47545fe
Kauri <kauri.vaab@gmail.com>
2025-06-18 15:49:02 +0300

    HW 5

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import models.Model;
 import views.View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) {
@@ -18,6 +19,7 @@ public class App {
 
                 view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Üleval paremal nupud toimima
                 view.pack();
+                view.setMinimumSize(new Dimension(755,380));
                 view.setLocationRelativeTo(null); //Keskele
                 view.setVisible(true); //Teeb Frame nähtavaks
             }
